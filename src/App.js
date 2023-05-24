09394acb4d576b51663e85750bd117782115bd5e
Seval Senturk <seval.senturk@tgs.aero>
2023-05-24 14:09:30 +0300

    state minik proje

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import Course from "./Course";
+import "./App.css";
+import { useState } from "react";
+
+function getRandomCourse() {
+  const courseArray = ["Angular", "Bootstrap", "Ccsharp", "KompleWeb"];
+  return courseArray[Math.floor(Math.random() * courseArray.length)];
+}
 
 function App() {
+  const [courses, setCourses] = useState([]);
+
+  const handleClick = () => {
+    setCourses([...courses, getRandomCourse()]);
+  };
+
+  const courseList = courses.map((course, index) => {
+    return <Course key={index} courseName={course} />;
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button className="appButton" onClick={handleClick}>Kurs Ekle</button>
+      <div className="courseList">{courseList}</div>
     </div>
   );
 }
---
+++ src/Course.js
@@ -0,0 +1,22 @@
+import Angular from './images/angular.jpg';
+import Bootstrap from './images/bootstrap5.png';
+import Ccsharp from './images/ccsharp.png';
+import KompleWeb from './images/kompleweb.jpg';
+import "./Course.css";
+
+const courseMap={
+    Angular,
+    Bootstrap,
+    Ccsharp,
+    KompleWeb
+}
+
+function Course({ courseName }) {
+  return (
+    <div className='CourseBox'>
+      <img className="courseImg" src={courseMap[courseName]} alt="course" />
+    </div>
+  );
+}
+
+export default Course;
